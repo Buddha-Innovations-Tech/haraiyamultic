f145b4c944f1481e605653619fa0e843f5633410
Akash Khattri <akash.khattri.ak@gmail.com>
2022-06-16 09:56:43 +0545

    adsad

--- src/containers/Photos/index.js
+++ src/containers/Photos/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import BannerImage from "../../images/banner-image.png";
-import Library from "../../images/library.jpg";
-import Classroom from "../../images/classroom.png";
-import Graduation from "../../images/graduation.png";
-import Event from "../../images/events.png";
-import Program from "../../images/program.png";
-import gql from "graphql-tag";
-import Query from "../../components/Query";
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import BannerImage from '../../images/banner-image.png';
+import Library from '../../images/library.jpg';
+import Classroom from '../../images/classroom.png';
+import Graduation from '../../images/graduation.png';
+import Event from '../../images/events.png';
+import Program from '../../images/program.png';
+import gql from 'graphql-tag';
+import Query from '../../components/Query';
 
 const Photos = () => {
   const GET_PHOTOS = gql`
@@ -16,9 +16,8 @@ const Photos = () => {
       photos {
         nodes {
           slug
+          title
           photos {
-            title
-
             featuredimage {
               sourceUrl
             }
@@ -36,25 +35,25 @@ const Photos = () => {
         {({ data: { photos } }) => {
           return (
             <>
-              <div className="banner-image">
-                <img src={BannerImage} alt="" className="img-banner" />
-                <div className="overlay">
-                  <div className="container">
-                    <div className="alumini-banner-content">
-                      <h1 className="alumini-heading">
+              <div className='banner-image'>
+                <img src={BannerImage} alt='' className='img-banner' />
+                <div className='overlay'>
+                  <div className='container'>
+                    <div className='alumini-banner-content'>
+                      <h1 className='alumini-heading'>
                         We'd love to hear from <br />
                         you
                       </h1>
-                      <nav aria-label="breadcrumb">
-                        <ol className="breadcrumb contact-breadcrumb">
-                          <li className="breadcrumb-item">
-                            <Link to="/Home" className="breadcrumb-item1">
+                      <nav aria-label='breadcrumb'>
+                        <ol className='breadcrumb contact-breadcrumb'>
+                          <li className='breadcrumb-item'>
+                            <Link to='/Home' className='breadcrumb-item1'>
                               Home
                             </Link>
                           </li>
                           <li
-                            className="breadcrumb-item active breadcrumb-item2"
-                            aria-current="page"
+                            className='breadcrumb-item active breadcrumb-item2'
+                            aria-current='page'
                           >
                             Photos
                           </li>
@@ -66,24 +65,24 @@ const Photos = () => {
               </div>
 
               <Container>
-                <div class="notices">
-                  <h1 class="notice-head">Photo Gallery</h1>
+                <div class='notices'>
+                  <h1 class='notice-head'>Photo Gallery</h1>
                 </div>
-                <div class="gallery-images">
-                  <div class="row gy-5">
+                <div class='gallery-images'>
+                  <div class='row gy-5'>
                     {photos.nodes?.map((a) => {
                       return (
                         <>
-                          <div class="col-md-4">
-                            <div class="photo-list">
+                          <div class='col-md-4'>
+                            <div class='photo-list'>
                               <img
                                 src={a.photos?.featuredimage?.sourceUrl}
-                                alt=""
-                                class="img-fluid"
+                                alt=''
+                                class='img-fluid'
                               />
-                              <div class="overlay">
+                              <div class='overlay'>
                                 <Link to={`/photosfull/${a.slug}`}>
-                                  <p class="img-text">{a.photos?.title}</p>
+                                  <p class='img-text'>{a.title}</p>
                                 </Link>
                               </div>
                             </div>
