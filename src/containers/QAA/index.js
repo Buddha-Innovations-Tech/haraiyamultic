ebe65ec2c9a22098c1c08aea7ef2ec2e96f515e3
Akash Khattri <akash.khattri.ak@gmail.com>
2022-09-25 12:31:16 +0545

    download fix

--- src/containers/QAA/index.js
+++ src/containers/QAA/index.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Container, Modal, Button } from "react-bootstrap";
-import BannerImage from "../../images/banner-image.png";
-import gql from "graphql-tag";
-import Query from "../../components/Query";
-import { BsFillFilePdfFill } from "react-icons/bs";
-import ScrollToTop from "../../components/ScrollToTop";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Modal, Button } from 'react-bootstrap';
+import BannerImage from '../../images/banner-image.png';
+import gql from 'graphql-tag';
+import Query from '../../components/Query';
+import { BsFillFilePdfFill } from 'react-icons/bs';
+import ScrollToTop from '../../components/ScrollToTop';
 
 const QAA = () => {
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState('');
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [downloadLink, setDownloadlink] = useState('');
+  const [linkPassword, setLinkPassword] = useState('');
   const GET_QAA = gql`
     query NewQuery {
       qaas {
@@ -37,25 +39,25 @@ const QAA = () => {
         {({ data: { qaas } }) => {
           return (
             <>
-              <div className="banner-image">
-                <img src={BannerImage} alt="" className="img-banner" />
-                <div className="overlay">
-                  <div className="container">
-                    <div className="alumini-banner-content">
-                      <h1 className="alumini-heading">
+              <div className='banner-image'>
+                <img src={BannerImage} alt='' className='img-banner' />
+                <div className='overlay'>
+                  <div className='container'>
+                    <div className='alumini-banner-content'>
+                      <h1 className='alumini-heading'>
                         We'd love to hear from <br />
                         you
                       </h1>
-                      <nav aria-label="breadcrumb">
-                        <ol className="breadcrumb contact-breadcrumb">
-                          <li className="breadcrumb-item">
-                            <Link to="/Home" className="breadcrumb-item1">
+                      <nav aria-label='breadcrumb'>
+                        <ol className='breadcrumb contact-breadcrumb'>
+                          <li className='breadcrumb-item'>
+                            <Link to='/Home' className='breadcrumb-item1'>
                               Home
                             </Link>
                           </li>
                           <li
-                            className="breadcrumb-item active breadcrumb-item2"
-                            aria-current="page"
+                            className='breadcrumb-item active breadcrumb-item2'
+                            aria-current='page'
                           >
                             QAA
                           </li>
@@ -65,10 +67,10 @@ const QAA = () => {
                   </div>
                 </div>
               </div>
-              <Container className="mt-5">
-                <table className="table">
+              <Container className='mt-5'>
+                <table className='table'>
                   <thead>
-                    <tr className="table-heading">
+                    <tr className='table-heading'>
                       <th>S.N</th>
                       <th>QAA</th>
                       <th>Download</th>
@@ -81,48 +83,21 @@ const QAA = () => {
                           {a?.qaa?.qaaname?.map((b, index) => {
                             return (
                               <>
-                                <tr className="table-body">
+                                <tr className='table-body'>
                                   <td>{index + 1}</td>
                                   <td>{b?.name}</td>
 
                                   <td>
                                     <BsFillFilePdfFill
-                                      className="fa-file-pdf"
-                                      onClick={handleShow}
+                                      className='fa-file-pdf'
+                                      onClick={(e) => {
+                                        handleShow();
+                                        setDownloadlink(
+                                          b?.downloadlink?.mediaItemUrl
+                                        );
+                                        setLinkPassword(b?.password);
+                                      }}
                                     />
-                                    <Modal show={show} onHide={handleClose}>
-                                      <Modal.Header closeButton>
-                                        <Modal.Title>
-                                          Enter Password
-                                        </Modal.Title>
-                                      </Modal.Header>
-                                      <Modal.Body>
-                                        <input
-                                          type="password"
-                                          className="form-control contact-placeholder downloadfile-input"
-                                          placeholder="Enter Password"
-                                          value={password}
-                                          onChange={(e) =>
-                                            setPassword(e.target.value)
-                                          }
-                                          required
-                                        />
-                                      </Modal.Body>
-                                      <Modal.Footer>
-                                        {b?.password === password ? (
-                                          <a
-                                            href={b?.downloadlink?.mediaItemUrl}
-                                            target="_blank"
-                                            onClick={handleClose}
-                                            className="downloadfile-btn"
-                                          >
-                                            Download
-                                          </a>
-                                        ) : (
-                                          ""
-                                        )}
-                                      </Modal.Footer>
-                                    </Modal>
 
                                     {/* <a
                                       href={b?.downloadlink?.mediaItemUrl}
@@ -139,6 +114,35 @@ const QAA = () => {
                     })}
                   </tbody>
                 </table>
+                <Modal show={show} onHide={handleClose}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Enter Password</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <input
+                      type='password'
+                      className='form-control contact-placeholder downloadfile-input'
+                      placeholder='Enter Password'
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                  </Modal.Body>
+                  <Modal.Footer>
+                    {linkPassword === password ? (
+                      <a
+                        href={downloadLink}
+                        target='_blank'
+                        onClick={handleClose}
+                        className='downloadfile-btn'
+                      >
+                        Download
+                      </a>
+                    ) : (
+                      ''
+                    )}
+                  </Modal.Footer>
+                </Modal>
               </Container>
             </>
           );
